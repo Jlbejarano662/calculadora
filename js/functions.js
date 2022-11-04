c45120354d866aafd5c6261c64a59c62505dae82
Jlbejarano662 <jasmin.1207@hotmail.com>
2022-11-04 15:45:35 -0500

    Versión final

--- js/functions.js
+++ js/functions.js
@@ -4,84 +4,130 @@ var operandob;
 var operacion;
 
 function init() {
-  //variables
+  //---------------------------variables------------------------------------
+  //selecciona el elemento con el id resultado y los asigna a la variable
   var resultado = document.getElementById("resultado");
+  //selecciona el elemento HTML con el id resultado y los asigna a la variable
   var reset = document.getElementById("reset");
+  //selecciona el elemento HTML con el id resultado y los asigna a la variable
   var suma = document.getElementById("suma");
+  //selecciona el elemento HTML con el id resta y los asigna a la variable
   var resta = document.getElementById("resta");
+  //selecciona el elemento HTML con el id multiplicacion y los asigna a la variable  
   var multiplicacion = document.getElementById("multiplicacion");
+  //selecciona el elemento HTML con el id division y los asigna a la variable  
   var division = document.getElementById("division");
+  //selecciona el elemento HTML con el id igual y los asigna a la variable  
   var igual = document.getElementById("igual");
+  //selecciona el elemento HTML con el id uno y los asigna a la variable  
   var uno = document.getElementById("uno");
+  //selecciona el elemento HTML con el id resta y los asigna a la variable  
   var dos = document.getElementById("dos");
+  //selecciona el elemento HTML con el id tres y los asigna a la variable  
   var tres = document.getElementById("tres");
+  //selecciona el elemento HTML con el id cuatro y los asigna a la variable  
   var cuatro = document.getElementById("cuatro");
+  //selecciona el elemento HTML con el id cinco y los asigna a la variable
   var cinco = document.getElementById("cinco");
+  //selecciona el elemento HTML con el id seis y los asigna a la variable
   var seis = document.getElementById("seis");
+  //selecciona el elemento HTML con el id siete y los asigna a la variable
   var siete = document.getElementById("siete");
+  //selecciona el elemento HTML con el id ocho y los asigna a la variable
   var ocho = document.getElementById("ocho");
+  //selecciona el elemento HTML con el id nueve y los asigna a la variable
   var nueve = document.getElementById("nueve");
+  //selecciona el elemento HTML con el id cero y los asigna a la variable
   var cero = document.getElementById("cero");
 }
 
-//Eventos de click a los botones de número
-uno.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "1";
+//-----------------Eventos de click a los botones de número--------------
+//Evento click que añade el valor 1 a la pantalla (variable resultado)
+uno.onclick = function () {
+  resultado.textContent += "1";
 };
-dos.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "2";
+//Evento click que añade el valor 2 a la pantalla (variable resultado)
+dos.onclick = function () {
+  resultado.textContent += "2";
 };
-tres.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "3";
+//Evento click que añade el valor 3 a la pantalla (variable resultado)
+tres.onclick = function () {
+  resultado.textContent += "3";
 };
-cuatro.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "4";
+//Evento click que añade el valor 4 a la pantalla (variable resultado)
+cuatro.onclick = function () {
+  resultado.textContent += "4";
 };
-cinco.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "5";
+//Evento click que añade el valor 5 a la pantalla (variable resultado)
+cinco.onclick = function () {
+  resultado.textContent += "5";
 };
-seis.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "6";
+//Evento click que añade el valor 6 a la pantalla (variable resultado)
+seis.onclick = function () {
+  resultado.textContent +="6";
 };
-siete.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "7";
+//Evento click que añade el valor 7 a la pantalla (variable resultado)
+siete.onclick = function () {
+  resultado.textContent += "7";
 };
-ocho.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "8";
+//Evento click que añade el valor 8 a la pantalla (variable resultado)
+ocho.onclick = function () {
+  resultado.textContent +="8";
 };
-nueve.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "9";
+//Evento click que añade el valor 9 a la pantalla (variable resultado)
+nueve.onclick = function () {
+  resultado.textContent += "9";
 };
-cero.onclick = function (e) {
-  resultado.textContent = resultado.textContent + "0";
+//Evento click que añade el valor 0 a la pantalla (variable resultado)
+cero.onclick = function () {
+  resultado.textContent += "0";
 };
-//Evento de click al botón de limpiar
-reset.onclick = function (e) {
+//Evento de click al botón de limpiar que ejecuta la función limpiar
+reset.onclick = function () {
   resetear();
 };
-//Eventos de click a los botones de operaciones
-suma.onclick = function (e) {
+//------Eventos de click a los botones de operaciones--------
+// Evento click que añade la función de suma
+suma.onclick = function () {
+  //Guarda el valor de primer número tecleado 
   operandoa = resultado.textContent;
+  //Establece que la operación es de suma
   operacion = "+";
+  //Limpia la pantalla
   limpiar();
 };
-resta.onclick = function (e) {
+//Evento click que añade la función de resta
+resta.onclick = function () {
+  //Guarda el valor de primer número tecleado 
   operandoa = resultado.textContent;
+  //Establece que la operación es de resta
   operacion = "-";
+  //Limpia la pantalla
   limpiar();
 };
-multiplicacion.onclick = function (e) {
+//Evento click que añade la función de multiplicación
+multiplicacion.onclick = function () {
+  //Guarda el valor de primer número tecleado 
   operandoa = resultado.textContent;
+  //Establece que la operación es de multiplicación
   operacion = "*";
+  //Limpia la pantalla
   limpiar();
 };
-division.onclick = function (e) {
+//Evento click que añade la función de divisón
+division.onclick = function () {
+  //Guarda el valor de primer número tecleado 
   operandoa = resultado.textContent;
+  //Establece que la operación es de divisón
   operacion = "/";
+  //Limpia la pantalla
   limpiar();
 };
-igual.onclick = function (e) {
+//Evento click al botón igual  
+igual.onclick = function () {
+  //Guarda el valor del segundo número tecleado
   operandob = resultado.textContent;
+  //Ejecuta la función resolver.
   resolver();
 };
 
@@ -97,7 +143,7 @@ function resetear() {
   operacion = "";
 }
 
-//Determina la operación a realizar 
+//Determina la operación a realizar y la ejecuta
 function resolver() {
   var res = 0;
   switch (operacion) {
@@ -114,7 +160,9 @@ function resolver() {
       res = parseFloat(operandoa) / parseFloat(operandob);
       break;
   }
+  //Deja en ceros la calculadora
   resetear();
+  //muestra el resultado en la pantalla
   resultado.textContent = res;
 }
 
